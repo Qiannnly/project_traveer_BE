38f8cd6ee0f332a0e7f1639fdfa18f55d8029558
Qiannnly <qianlinggg@gmail.com>
2025-02-27 23:54:49 +0800

    removed unused test and production database configurations

--- src/config/database.js
+++ src/config/database.js
@@ -8,18 +8,4 @@ module.exports = {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
   },
-  // "test": {
-  //   "username": "root",
-  //   "password": null,
-  //   "database": "database_test",
-  //   "host": "127.0.0.1",
-  //   "dialect": "mysql"
-  // },
-  // "production": {
-  //   "username": "root",
-  //   "password": null,
-  //   "database": "database_production",
-  //   "host": "127.0.0.1",
-  //   "dialect": "mysql"
-  // }
 };
